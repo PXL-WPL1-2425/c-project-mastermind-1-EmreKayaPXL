a97a2ebc4a4740cb7c20f7470872a1c22309e58e
EmreKayaPXL <12400659@student.pxl.be>
2024-11-15 19:42:12 +0100

    MasterMind-02

--- Mastermind_WPL/Mastermind_WPL/MainWindow.xaml.cs
+++ Mastermind_WPL/Mastermind_WPL/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Schema;
 
 namespace Mastermind_WPL
 {
@@ -20,18 +21,53 @@ namespace Mastermind_WPL
     /// </summary>
     public partial class MainWindow : Window
     {
-        
 
         public MainWindow()
         {
             InitializeComponent();
+            titleRandomColors();
         }
 
+        private void titleRandomColors()
+        {
+            // alle kleuren
+            string[] colors = { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
+            Random random = new Random();
 
+            // StringBuilder voor tietel
+            StringBuilder titleBuilder = new StringBuilder("Mastermind kleur: ");
+
+            // Willekeurige kleuren selecteren
+            for (int i = 0; i < 4; i++)
+            {
+                int randomIndex = random.Next(0, colors.Length);
+                titleBuilder.Append(colors[randomIndex]);
+
+                // Voeg een komma en spatie toe behalve na de laatste kleur
+                if (i < 3)
+                {
+                    titleBuilder.Append(", ");
+                }
+            }
+
+            // Stel de titel van het venster in
+            this.Title = titleBuilder.ToString();
+        }
 
         private void CheckButton_Click(object sender, RoutedEventArgs e)
         {
-           
+
         }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
