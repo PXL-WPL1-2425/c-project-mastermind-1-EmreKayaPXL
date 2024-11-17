18d90fdebb66125a80c5b8d2eee0ea30e0bc2dda
EmreKayaPXL <12400659@student.pxl.be>
2024-11-17 15:36:15 +0100

    MasterMind-05

--- Mastermind_WPL/Mastermind_WPL/MainWindow.xaml.cs
+++ Mastermind_WPL/Mastermind_WPL/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -50,13 +51,9 @@ namespace Mastermind_WPL
                 }
             }
 
+
             // Stel de titel van het venster in
             this.Title = titleBuilder.ToString();
-        }
-
-        private void CheckButton_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void ComboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -64,42 +61,138 @@ namespace Mastermind_WPL
             if (ComboBox1.SelectedItem is ComboBoxItem ComboBox1Item)  // als item is geselecteerd
             {
                 if (ComboBox1Item.Background is SolidColorBrush Kleur) // dan: kleur borstel naar achtergrondskleur van geselecteerde Item
-                { 
+                {
                     label1.Background = Kleur;   //label de keur geven van achtergrondskleur van geselecteerde Item  
+                    label1.Content = ComboBox1Item.Content.ToString();
                 }
             }
         }
 
         private void ComboBox2_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (ComboBox2.SelectedItem is ComboBoxItem ComboBox2Item) 
+            if (ComboBox2.SelectedItem is ComboBoxItem ComboBox2Item)
             {
-                if (ComboBox2Item.Background is SolidColorBrush Kleur) 
+                if (ComboBox2Item.Background is SolidColorBrush Kleur)
                 {
-                    label2.Background = Kleur;   
+                    label2.Background = Kleur;
+                    label2.Content = ComboBox2Item.Content.ToString();
                 }
             }
         }
 
         private void ComboBox3_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (ComboBox3.SelectedItem is ComboBoxItem ComboBox3Item) 
+            if (ComboBox3.SelectedItem is ComboBoxItem ComboBox3Item)
             {
-                if (ComboBox3Item.Background is SolidColorBrush Kleur) 
+                if (ComboBox3Item.Background is SolidColorBrush Kleur)
                 {
-                    label3.Background = Kleur;   
+                    label3.Background = Kleur;
+                    label3.Content = ComboBox3Item.Content.ToString();
                 }
             }
         }
 
         private void ComboBox4_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (ComboBox4.SelectedItem is ComboBoxItem ComboBox4Item) 
+            if (ComboBox4.SelectedItem is ComboBoxItem ComboBox4Item)
             {
-                if (ComboBox4Item.Background is SolidColorBrush Kleur) 
+                if (ComboBox4Item.Background is SolidColorBrush Kleur)
                 {
-                    label4.Background = Kleur;   
+                    label4.Background = Kleur;
+                    label4.Content = ComboBox4Item.Content.ToString();
                 }
+            }
+        }
+
+         
+
+
+        private void CheckButton_Click(object sender, RoutedEventArgs e)
+        {
+            string[] titleColors = this.Title.Split(':')[1].Split(',');
+
+
+
+
+            int MasterMindStrenghtNumber = 0;
+
+            string label1Color = label1.Content.ToString(); 
+            string label2Color = label2.Content.ToString();
+            string label3Color = label3.Content.ToString();
+            string label4Color = label4.Content.ToString();
+
+            if (this.Title.Contains(label1Color))
+            {
+                MasterMindStrenghtNumber++;
+                if (titleColors[0].Contains(label1Color))
+                {
+                    label1.BorderBrush = Brushes.DarkRed;
+                }else 
+                {
+                    label1.BorderBrush = Brushes.Wheat;
+                }
+            }
+
+            if (this.Title.Contains(label2Color))
+            {
+                MasterMindStrenghtNumber++;
+                if (titleColors[1].Contains(label2Color))
+                {
+                    label2.BorderBrush = Brushes.DarkRed;
+                }
+                else
+                {
+                    label2.BorderBrush = Brushes.Wheat;
+                }
+            }
+
+            if (this.Title.Contains(label3Color))
+            {
+                MasterMindStrenghtNumber++;
+                if (titleColors[2].Contains(label3Color))
+                {
+                    label3.BorderBrush = Brushes.DarkRed;
+                }
+                else
+                {
+                    label3.BorderBrush = Brushes.Wheat;
+                }
+            }
+     
+            if (this.Title.Contains(label4Color))
+            {
+                MasterMindStrenghtNumber++;
+                if (titleColors[3].Contains(label4Color))
+                {
+                    label4.BorderBrush = Brushes.DarkRed;
+                }
+                else
+                {
+                    label4.BorderBrush = Brushes.Wheat;
+                }
+            }
+
+
+
+
+            switch (MasterMindStrenghtNumber)
+            {
+                case 4:
+                    resultTextBlock.Text = "4 kleuren komen voor";
+                    break;
+                case 3:
+                    resultTextBlock.Text = "3 kleuren komen voor";
+                    break;
+                case 2:
+                    resultTextBlock.Text = "2 kleur komt voor";
+                    break;
+                case 1:
+                    resultTextBlock.Text = "1 kleur komt voor";
+                    break;
+                default:
+                    resultTextBlock.Text = "Niet de juiste kleuren gebruikt";
+                    break;
+
             }
         }
     }
